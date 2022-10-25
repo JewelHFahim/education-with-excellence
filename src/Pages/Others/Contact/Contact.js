f7e263af2483ef272bd638ea9cfb4d19a9aa331d
JewelHFahim <jewelhfahim@gmail.com>
2022-10-25 21:00:27 +0600

    contact page

--- src/Pages/Others/Contact/Contact.js
+++ src/Pages/Others/Contact/Contact.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { FaMapMarked, FaEnvelopeOpenText, FaPhoneAlt } from "react-icons/fa";
+import {
+  FaMapMarked,
+  FaEnvelopeOpenText,
+  FaPhoneAlt,
+  FaMailBulk,
+} from "react-icons/fa";
 
 const Contact = () => {
   return (
@@ -7,10 +12,10 @@ const Contact = () => {
       <div className="w-9/12 mx-auto text-5xl text-center mt-8">
         <p>Contact Us</p>
       </div>
-
+      <div className="divider">with</div>
       <div className="grid grid-cols-3 gap-5 w-9/12 mx-auto mt-5">
         <div className="card  bg-base-100 shadow-xl border border-slate-200 p-2 m-5  items-center">
-          <FaMapMarked className="text-8xl text-orange-900" />
+          <FaMapMarked className="text-8xl text-[#FF5349]" />
           <div className="card-body items-center text-center">
             <h2 className="card-title">Address</h2>
             <p>228-5 Main Street, Georgia, USA</p>
@@ -18,7 +23,7 @@ const Contact = () => {
         </div>
 
         <div className="card  bg-base-100 shadow-xl border border-slate-200 p-2 m-5  items-center">
-          <FaEnvelopeOpenText className="text-8xl text-orange-900" />
+          <FaEnvelopeOpenText className="text-8xl text-[#FF5349]" />
           <div className="card-body items-center text-center">
             <h2 className="card-title">Email Address</h2>
             <p>contact@educavo.com</p>
@@ -26,12 +31,23 @@ const Contact = () => {
         </div>
 
         <div className="card  bg-base-100 shadow-xl border border-slate-200 p-2 m-5  items-center">
-          <FaPhoneAlt className="text-8xl text-orange-900" />
+          <FaPhoneAlt className="text-8xl text-[#FF5349]" />
           <div className="card-body items-center text-center">
             <h2 className="card-title">Phone Number</h2>
             <p>(+01)999-999-4444</p>
           </div>
         </div>
+      </div>
+      <div className="bg-slate-900 w-9/12 mx-auto p-8 mt-4 text-center flex justify-around ">
+        <div className="flex items-center">
+          <FaMailBulk  className="text-6xl mr-2 text-[#FF5349]"/>
+          <p className="text-white text-4xl "> Subscribe to Newsletter</p>
+        </div>
+        <input
+          className="input input-bordered input-success w-full max-w-xs"
+          type="email"
+          placeholder="Email"
+        />
       </div>
     </div>
   );
